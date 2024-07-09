73730c3be97562daac3e8de29e4462cd03fa117a
Inma Quesada Tirado <nihvix.art@gmail.com>
2024-07-09 19:42:45 +0200

    Base JS (doesn't work yet;-;)

--- app.js
+++ app.js
@@ -5,11 +5,38 @@
 /*====================
         VARIABLES  
   ====================*/
-
+var draggableItems;
+var productImg;
+var productCart;
 /*====================
       FUNCIONES  
 ====================*/
 
-/*====================
-      EVENTOS  
-====================*/
+function productMoving(event) {
+      productImg = event.target;
+      console.log("moving");
+}
+
+
+/* ========================
+      EVENTO PRINCIPAL  
+   ========================*/
+document.addEventListener('DOMContentLoaded', (ev) => {
+      //Añadimos la funcionalidad de movimiento (drag) a todos los productos
+      draggableItems = document.getElementsByClassName("draggableItem");
+      for (let item of draggableItems) {
+            item.addEventListener('dragstart', productMoving);
+      }
+      //Añadimos la funcionalidad sobre los contenedores del carrito para soltar los productos
+      productCart = document.getElementsByClassName("product");
+      for (let item of productCart) {
+            item.addEventListener('dragover', e => { e.preventDefault(); });
+            item.addEventListener('drop', (item) => {
+                  console.log("item src: "+item.src);
+                  item.src = productImg.src;
+                  console.log("item src: "+item.src);
+                  console.log("productImg src: "+productImg.src);
+                  console.log("change");
+            });
+      }
+});
