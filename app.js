9b4bb3a15d33d9b144bf6bcfb2fe48a441f09fed
Inma Quesada Tirado <nihvix.art@gmail.com>
2024-07-10 23:32:24 +0200

    Ticket text (price still on work)

--- app.js
+++ app.js
@@ -5,15 +5,21 @@
 /*====================
         VARIABLES  
   ====================*/
-var draggableItems;
-var productImg;
-var productCart;
+var draggableItems; //elementos de la izq
+var productImg; //producto en movimiento de la izq
+var productCart; //array con todos los productos del carrito
+var ticketList; //texto del ticket
+var totalPrice; //texto con el precio total
+var sum; //suma para actualizar el precio total
+var productPrice;
 /*====================
       FUNCIONES  
 ====================*/
 
 function productMoving(event) {
       productImg = event.target;
+      productPrice=productImg.getElementsByClassName("priceProduct");
+      console.log("product price: "+productPrice);
       console.log("moving");
 }
 
@@ -22,6 +28,12 @@ function productMoving(event) {
       EVENTO PRINCIPAL  
    ========================*/
 document.addEventListener('DOMContentLoaded', (ev) => {
+      //Variables para cambiar el texto del ticket
+      ticketList = document.getElementById("ticketList");
+      ticketList.innerText="";
+      totalPrice = document.getElementsByClassName("totalPrice");
+      sum = 0;
+
       //Añadimos la funcionalidad de movimiento (drag) a todos los productos
       draggableItems = document.getElementsByClassName("draggableItem");
       for (let item of draggableItems) {
@@ -32,11 +44,13 @@ document.addEventListener('DOMContentLoaded', (ev) => {
       for (let item of productCart) {
             item.addEventListener('dragover', e => { e.preventDefault(); });
             item.addEventListener('drop', (item) => {
-                  console.log("item src: "+item.src);
-                  item.target.src = productImg.src;
-                  console.log("item src: "+item.src);
-                  console.log("productImg src: "+productImg.src);
-                  console.log("change");
+                  item.target.src = productImg.src; //Cambiamos la imagen del carrito por la seleccionada
+                  //Añadimos el producto al ticket
+                  console.log(ticketList.innerText);
+                  ticketList.innerText+="** "+productImg.title+"..."+productPrice.innerText+"€\n";
+                  sum+=parseInt(productPrice.innerText);
+                  totalPrice.innerText=sum;
+                  console.log(ticketList.innerText);
             });
       }
 });
